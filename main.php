3cbadcb9f0936058a1973940c7d3bdd8b4a2d598
carlos93263 <93263.joan23@fje.edu>
2016-01-27 12:15:01 +0100

    Subir informacion para coordinacion grupal

---
+++ main.php
@@ -0,0 +1,43 @@
+<?php
+	//iniciamos sesión - SIEMPRE TIENE QUE ESTAR EN LA PRIMERA LÍNEA
+	session_start();
+?>
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+	    <meta charset="utf-8" />
+	    <title></title>
+	    <link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<?php
+		//AQUIIIIIIIIIIIIIIIIIIIIIIIIIII INVERTIIIIIIIIIIR EL ISSET CUANDO TENGAMOS SESSIONES WORKING!
+		if(!isset($_SESSION['mail'])){
+			include("procs/conexion.proc.php");
+		?>
+	        <header id="header_main">
+	        	<button onclick="window.location.href='crear_contacto.php'">+ Añadir Contacto</button>
+	        	<button onclick="window.location.href='procs/logout.proc.php'">Cerrar Sesión</button>
+	        </header>
+	        <section class="cuerpo_section">
+		        <!-- ARTICLE DE LISTA DE CONTACTOS -->
+			   	<article>
+			    	<h1>Lista de contactos:</h1>
+			    </article>
+				<!-- ARTICLE DE MAPA DE CADA CONTACTO -->
+			    <article>
+			    	<h1>Información del contacto:</h1>
+			    </article>
+		    </section>
+
+	        <footer>
+	        	Derechos reservados a Carlos y Daniel. DAW-2
+	        </footer>
+	    <?php
+	    } else {
+	    	$_SESSION['error']="No has iniciado sesion todavía. Si no tienes una cuenta prueba a registrarte.";
+			header("location: index.php");
+	    }
+        ?>
+	</body>
+</html>
