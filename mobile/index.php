54c7006db92fd5ada7f152285e864cb1f654e1ac
carlos93263 <93263.joan23@fje.edu>
2016-01-27 09:12:48 +0100

    Subida estructura definitiva + comprovacion mobil o PC

---
+++ mobile/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+	<head>
+	    <meta charset="utf-8" />
+	    <title>index</title>
+	</head>
+	<body>
+        <header>
+            <nav>
+            </nav>
+        </header>
+	        Holita
+        <footer>
+        </footer>
+	</body>
+</html>
