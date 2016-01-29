74bee939ef9ec8f73b6d5eaedbe7a1f8c5510d30
carlos93263 <93263.joan23@fje.edu>
2016-01-29 12:48:19 +0100

    subida definitiva de proyecto

--- main.php
+++ main.php
@@ -1,13 +1,15 @@
 <?php
 	//iniciamos sesión - SIEMPRE TIENE QUE ESTAR EN LA PRIMERA LÍNEA
 	session_start();
+	include("proc/connection.proc.php");
 ?>
 <!DOCTYPE html>
 <html lang="es">
 	<head>
-	    <meta charset="utf-8" />
+	    <meta charset="utf-8"/>
 	    <title></title>
-	    <link rel="stylesheet" href="css/style.css">
+	    <link rel="stylesheet" href="css/style_main.css">
+	    <script type="text/javascript" src="js/script_main.js"></script>
 	</head>
 	<body>
 		<?php
@@ -16,22 +18,118 @@
 			include("proc/connection.proc.php");
 		?>
 	        <header id="header_main">
-	        	<button onclick="window.location.href='crear_contacto.php'">+ Añadir Contacto</button>
-	        	<button onclick="window.location.href='proc/logout.proc.php'">Cerrar Sesión</button>
-	        	<button onclick="window.location.href='modificar_perfil.php'">Modificar Perfil</button>
+	        	<button onclick="window.location.href='crear_contacto.php'">+ Añadir Contacto</button><br/>
+	        	<button onclick="window.location.href='modificar_perfil.php'">Modificar Perfil</button><br/>
+	        	<?php echo utf8_encode($_SESSION['name']); ?><br/><button onclick="window.location.href='proc/logout.proc.php'">Cerrar Sesión</button>
 	        </header>
-	        <section class="cuerpo_section">
-		        <!-- ARTICLE DE LISTA DE CONTACTOS -->
-			   	<article>
-			    	<h1>Lista de contactos:</h1>
-			    </article>
-				<!-- ARTICLE DE MAPA DE CADA CONTACTO -->
-			    <article>
-			    	<h1>Información del contacto:</h1>
-			    </article>
-		    </section>
+	        <section class="contenedor_central">
+		    	<section class="contactos">
+		    		<h1 id="menu_contactos">Contactos:</h1>
+		    		<?php
+		    		//CONSULTA QUE BUSCARA LOS CONTACTOS PARA LISTARLOS POSTERIORMENTE.
+		    		$sql_contactos="SELECT * FROM tbl_contacts WHERE tbl_contacts.usu_mail='$_SESSION[mail]'";
+		    		$datos_contactos = mysqli_query($con,$sql_contactos);
 
-	        <footer>
+		    		if(mysqli_num_rows($datos_contactos)>0){
+		    			while($contacto=mysqli_fetch_array($datos_contactos)){
+		    		?>
+		    				<section class="contacto">
+		    					<article>
+		    						<header>
+		    							<?php echo "<h1 id='tit_con'>".utf8_encode($contacto['con_name']) . " " . utf8_encode($contacto['con_surname'])."</h1>"?>
+		    						</header>
+
+		    							<?php
+		    							if(($contacto['usu_mail']==$_SESSION['mail']) || ($_SESSION['level']=="admin" || ($_SESSION['level']=="root"))){
+												echo "<a href='modificar_contacto.php?con_id=".$contacto['con_id']."'>Modificar</a> - <a href='eliminar_contacto.php?con_id=".$contacto['con_id']."'>Eliminar</a>";
+										}
+										?>
+										<br/><br/>
+		    						<footer>
+		    							<!-- EN EL FOOTER CARGAR BOTON QUE RELLENE EL SECTION INFO_CONTACTOS (LADO DERECHO)-->
+		    							<!--<button onclick="rellenarInfoCon(<?php/* echo $contacto['con_id'] . ', ' . $contacto['con_mail'] . ', ' . $contacto['con_phone'] . ', ' . $contacto['con_phone2'] . ', ' . $contacto['con_lat_home'] . ', ' . $contacto['con_len_home'] . ', ' . $contacto['con_lat_other'] . ', ' . $contacto['con_len_other'] . ', ' . $contacto['con_text'] . ', ' . $contacto['con_name'] . ', ' . $contacto['con_surname'];*/ ?>)">+ Información</button>-->
+		    							<button onclick="window.location.href='main.php?con_id=<?php echo $contacto["con_id"] ?>'">+ Información</button>
+		    							<br/><br/>
+		    						</footer>
+		    					</article>
+		    				</section>
+		    		<?php
+		    			}//end while($actividad=mysqli_fetch_array($datos_actividades)){
+		    		} else {
+		    			?>
+		    			<section class="contacto">
+	    					<article>
+	    						<button onclick="window.location.href='crear_contacto.php'">+ Añadir Contacto</button>
+	    					</article>
+		    			</section>
+		    		<?php
+
+		    		}//end if(mysqli_num_rows($datos_actividades>0)){
+
+		    		?>
+		    	</section>
+		    	<section class="info_contactos">
+		    		<h1 id="menu_info_contactos">Información de los contactos:</h1>
+		    		<section class="muestra_contenido_contacto">
+	    			<?php 
+	    				if(isset($_REQUEST['con_id'])){
+							//echo $_REQUEST['con_id'];
+							$sql_info_contacto="SELECT * FROM tbl_contacts WHERE tbl_contacts.con_id='$_REQUEST[con_id]'";
+		    				$datos_con = mysqli_query($con,$sql_info_contacto);
+		    		?>
+		    			<section>
+		    				<form name='info_contacto'>
+		    				<?php
+			    				while($info=mysqli_fetch_array($datos_con)){
+			    					echo "Nombre y Apellidos:<br/>";
+			    					echo '<input type="text" id="name_form" value="' . $info["con_name"] .' '. $info["con_surname"] . '" disabled><br/><br/>';
+			    					echo "Teléfono 1:<br/>";
+			    					echo '<input type="text" id="phone_form" value="' . $info["con_phone"] . '" disabled><br/><br/>';
+			    					echo "Teléfono 2:<br/>";
+			    					echo '<input type="text" id="phone2_form" value="' . $info["con_phone2"] . '" disabled><br/><br/>';
+			    					echo "Mail:<br/>";
+			    					echo '<input type="text" id="mail_form" value="' . $info["con_mail"] . '" disabled><br/><br/>';
+			    					echo "Descripción:<br/>";
+			    					echo '<input type="text" id="mail_form" value="' . $info["con_text"] . '" disabled><br/><br/>';
+								}
+			    			?>
+		    				</form>
+		    			</section>
+		    			<h1>Ubicación 1:</h1>
+			    		<div id="map"></div>
+			    		<h1>Ubicación 2:</h1>
+			    		<div id="map2"></div>
+		    		<?php
+						} else {
+	    			?>
+	    				<section><!-- SECTION PARA EL FORMULARIO -->
+	    					<form name="info_contacto">
+			    				Nombre y Apellidos:<br/>
+			    				<input type="text" id="name_form" disabled><br/><br/>
+			    				Teléfono 1:<br/>
+			    				<input type="text" name="phone_form" disabled><br/><br/>
+			    				Teléfono 2:<br/>
+			    				<input type="text" name="phone2_form" disabled><br/><br/>
+			    				Mail:<br/>
+			    				<input type="mail" name="mail_form" disabled><br/><br/>
+			    				Descripción:<br/>
+			    				<input type="text" name="text_form" disabled><br/><br/>
+			    				
+			    			</form>
+			    			<h1>Ubicación 1:</h1>
+			    			<div id="map"></div>
+				    		<h1>Ubicación 2:</h1>
+				    		<div id="map2"></div>
+			    		</section>
+	    			
+				    		
+		    		<?php
+	    				}
+	    			?>
+	    			</section>
+		    	</section>
+		    </section>
+	        <footer id="pie">
 	        	Derechos reservados a Carlos y Daniel. DAW-2
 	        </footer>
 	    <?php
--- proc/connection.proc.php
+++ proc/connection.proc.php
@@ -1,5 +1,14 @@
 <?php 
 
+/*$con = mysqli_connect('mysql.hostinger.es', 'u643215434_admin', '1234567890!', 'u643215434_tacts');
+
+if (!$con) {
+	echo "No se ha podido establecer conexión con la base de datos".PHP_EOL;
+	exit;
+}*/
+
+
+
 $con = mysqli_connect('localhost', 'root', '', 'bd_mycontact');
 
 if (!$con) {
